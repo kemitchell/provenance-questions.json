d3e3086cc00893645d02e731f6744e098818cb0e
Kyle Mitchell <kyle@kemitchell.com>
2015-12-29 18:06:42 -0800

    Add comments to build script

--- build.js
+++ build.js
@@ -1,22 +1,25 @@
 var data = require('country-data').countries.all
-var lispy = require('lispy-json')
 
-process.stdin.resume()
-
+// Read standard input.
 var buffers = [ ]
-
 process.stdin
+  .resume()
   .on('data', function(buffer) {
     buffers.push(buffer) })
   .on('end', function() {
+    // Write to standard input.
     process.stdout.write(
-      lispy(
+      require('lispy-json')(
+        // Parse the template.
         JSON.parse(
           Buffer.concat(buffers)
             .toString()
+            // Replace placeholder for country names with JSON array.
             .replace(
               '"$COUNTRIES"',
               JSON.stringify(
+                // Map country data in country-data, extracting only
+                // country names.
                 Object.keys(data)
                   .map(function(key) {
                     return data[key].name })))))) })
