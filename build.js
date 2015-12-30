64eb1520ce04f6e84cc9d701e730cf0f5dc84306
Kyle Mitchell <kyle@kemitchell.com>
2015-12-29 18:56:14 -0800

    Validate exported data against a schema

--- build.js
+++ build.js
@@ -7,19 +7,23 @@ process.stdin
   .on('data', function(buffer) {
     buffers.push(buffer) })
   .on('end', function() {
-    // Write to standard input.
-    process.stdout.write(
-      require('lispy-json')(
-        // Parse the template.
-        JSON.parse(
-          Buffer.concat(buffers)
-            .toString()
-            // Replace placeholder for country names with JSON array.
-            .replace(
-              '"$COUNTRIES"',
-              JSON.stringify(
-                // Map country data in country-data, extracting only
-                // country names.
+    // Bundle embedded schemas.
+    require('json-schema-ref-parser').bundle(
+      // Parse the template.
+      JSON.parse(
+        Buffer.concat(buffers)
+          .toString()
+          // Replace placeholder for country names with JSON array.
+          .replace(
+            '"$COUNTRIES"',
+            JSON.stringify(
+              // Map country data in country-data, extracting only
+              // country names.
+              require('array-uniq')(
                 Object.keys(data)
                   .map(function(key) {
-                    return data[key].name })))))) })
+                    return data[key].name }))))),
+      function(error, schema) {
+        // Write to standard input.
+        process.stdout.write(
+          require('lispy-json')(schema)) }) })
--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-var questionnaire = require('./')
 var tape = require('tape')
+var questionnaire = require('./')
+
 var values = require('object-values')
 var variables = require('boolean-json-variables')
 
-tape(function(test) {
+var tv4 = require('tv4')
+var http = require('http')
+
+var draft04 = 'http://json-schema.org/draft-04/schema'
+
+tape('provenance-questions', function(test) {
   test.test('variables', function(test) {
     var variablesInPrompts = questionnaire.prompts
       .map(function(prompt) { return prompt.variable })
@@ -12,4 +18,20 @@ tape(function(test) {
         test.assert(
           ( variablesInPrompts.indexOf(variable) >= 0 ),
           'variable "' + variable + '" is set by a prompt') }) })
+
+  tape.test('validates against schema', function(test) {
+    http.get(draft04, function(response) {
+      var buffers = [ ]
+      response
+        .on('data', function(buffer) {
+          buffers.push(buffer) })
+        .on('end', function() {
+          tv4.addSchema(draft04, JSON.parse(Buffer.concat(buffers)))
+          test.same(
+            tv4.validateMultiple(require('./'), questionnaire).errors,
+            [ /* no errors */ ],
+            'validates without error')
+          test.end() }) }) })
+
     test.end() }) })
+
